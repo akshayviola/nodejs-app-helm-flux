edb4694599f6d11f458f3db9eb009f3a98bb5ee2
akshayviola <akshaysunil201@gmail.com>
2024-08-06 11:46:54 +0530

    modified

--- nodejs-app/index.js
+++ nodejs-app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('ta da it works!! this is the recent image, hello from flux !!!');
+  res.send('ta da it works!! this is the recent image, hello from flux !!');
 });
 
 app.listen(port, () => {
