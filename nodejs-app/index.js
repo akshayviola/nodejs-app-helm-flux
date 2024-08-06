f86b4ad0ec1ac2c5032a7296e29b799893269449
akshayviola <akshaysunil201@gmail.com>
2024-08-06 14:44:48 +0530

    test5

--- nodejs-app/index.js
+++ nodejs-app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('ta da it works!! this is the recent image hello from fluxcd');
+  res.send('ta da it works!! this is the recent, image hello from fluxcd ha ha ha  ');
 });
 
 app.listen(port, () => {
